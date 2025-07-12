abd26b9a2552fc1dd24b932fd8b50eae7791fa6a
uzo-chukwu <israeljohnson0464@gmail.com>
2025-07-12 11:21:01 +0100

    chore: restrict articles endpoint to read-only access

--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,6 @@ from rest_framework import viewsets
 from .models import Article
 from .serializers import ArticleSerializer
 
-class ArticleViewSet(viewsets.ModelViewSet):
+class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Article.objects.all().order_by('-created_at')
     serializer_class = ArticleSerializer
