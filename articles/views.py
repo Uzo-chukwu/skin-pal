c7af04ffb6fdedb0c727f815a78e1acc2d8328b9
uzo-chukwu <israeljohnson0464@gmail.com>
2025-07-12 08:34:14 +0100

    test: verify POST and GET for articles API

--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,6 @@ from rest_framework import viewsets
 from .models import Article
 from .serializers import ArticleSerializer
 
-class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
+class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all().order_by('-created_at')
     serializer_class = ArticleSerializer
